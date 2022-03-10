6f1cb4f32014fd2392f4a80e508dc767bcd71390
August Bonds <august.bonds@protonmail.com>
2022-03-10 22:59:11 +0200

    start outputs to /dev/null to avoid hanging in ApplicationStart

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /webapps/chess
 echo "This version has an if!"
 if [ ! -f "chess.pid" ]; 
 then
-  java -jar chess-0.0.1-SNAPSHOT.jar > chess.log &
+  java -jar chess-0.0.1-SNAPSHOT.jar > chess.log 2> /dev/null < /dev/null &
   echo $! > chess.pid
 fi
 exit 0
