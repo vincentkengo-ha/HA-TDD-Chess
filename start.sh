d8de7cc179f61dc5843749c6a53d5fbf27101638
August Bonds <august.bonds@protonmail.com>
2022-03-10 22:38:59 +0200

    fix mistake in start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /webapps/chess
 echo "This version has an if!"
 if [ ! -f "chess.pid" ]; 
 then
-  java -jar target/chess-0.0.1-SNAPSHOT.jar > chess.log &
+  java -jar chess-0.0.1-SNAPSHOT.jar > chess.log &
   echo $! > chess.pid
 fi
 
