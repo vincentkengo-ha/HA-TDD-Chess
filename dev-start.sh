8475a24701e656f50da00edd0901bad314fb2f6a
August Bonds <august.bonds@protonmail.com>
2022-03-10 21:34:15 +0200

    don't try to start in case there's already a pid

--- dev-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -f chess.pid ]; then
-  java -jar target/chess-0.0.1-SNAPSHOT.jar > chess.log &
-  echo $! > chess.pid
-fi
-
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /webapps/chess
-java -jar chess-0.0.1-SNAPSHOT.jar > chess.log &
+if [ ! -f chess.pid ]; then
+  java -jar target/chess-0.0.1-SNAPSHOT.jar > chess.log &
+  echo $! > chess.pid
+fi
 
-echo $! > chess.pid
