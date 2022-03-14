afaa80bce052eb23c502a4f9d512d38799e3ae8b
August Bonds <august.bonds@protonmail.com>
2022-03-14 22:31:29 +0200

    extend validateservice timeout

--- scripts/ValidateService.sh
+++ scripts/ValidateService.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NEXT_WAIT_TIME=0
-until [ $NEXT_WAIT_TIME -eq 5 ] || wget localhost:8080; do
+until [ $NEXT_WAIT_TIME -eq 10 ] || wget localhost:8080; do
     sleep $(( NEXT_WAIT_TIME++ ))
 done
-[ $NEXT_WAIT_TIME -lt 5 ]
+[ $NEXT_WAIT_TIME -lt 10 ]
